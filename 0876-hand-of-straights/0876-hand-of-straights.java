6bbdec8a6e29b73846be919159bc9e7a0892d298
Abhyuday1904 <124274509+Abhyuday1904@users.noreply.github.com>
2024-06-06 18:18:06 +0530

    Time: 114 ms (6.29%), Space: 45.5 MB (52.37%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.java
+++ 0876-hand-of-straights/0876-hand-of-straights.java
@@ -6,13 +6,12 @@ class Solution {
         for (int i = 0; i < arr.length; i++) {
             list.add(arr[i]);
         }
-        //    System.out.println(list);
+        Collections.sort(list);
         while (list.size() > 0) {
-            Collections.sort(list);
+            
             int a = k;
-       //     System.out.println(list);
             int curr = list.get(0);
-       //     System.out.println(curr);
+ 
             while (a > 1) {
 
                 if (!list.contains(curr + 1))
