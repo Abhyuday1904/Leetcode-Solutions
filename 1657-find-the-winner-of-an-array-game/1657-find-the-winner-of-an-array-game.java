c551adee7d01bb3d0c3b9fb349f9eb1e69cea551
Abhyuday1904 <124274509+Abhyuday1904@users.noreply.github.com>
2024-06-10 14:05:52 +0530

    Time: 0 ms (100%), Space: 59.4 MB (64.05%) - LeetHub

--- 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.java
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.java
@@ -1,34 +1,19 @@
 class Solution {
     public int getWinner(int[] arr, int k) {
-          Stack<Integer> st = new Stack<>();
-        Queue<Integer> q = new LinkedList<>();
-        int max = Integer.MIN_VALUE;
-        for (int i = 1; i < arr.length; i++) {
-        
-            q.add(arr[i]);
-            if(max<arr[i]) max = arr[i];
+        int count=0;
+        int winner=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(winner > arr[i]){
+                count++;
+            }
+            else{
+                winner=arr[i];
+                count=1;
+            }
+            if(count==k){
+                return winner;
+            }
         }
-        if(max<arr[0]) max = arr[0];
-        if(k>=arr.length) return max;
-        int count = 0;
-        st.push(arr[0]);
-       while(count<=k){
-        if (st.peek() > q.peek()) {
-            count++;
-            q.add(q.remove());
-        } else {
-            count = 0;
-            q.add(st.pop());
-
-            st.push(q.remove());
-            count++;
-
-        }
-        if (count >= k)
-            return st.peek();
-
-       }
-
-        return 0;
+        return winner;
     }
 }
