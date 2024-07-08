ca50856f7b021637cc0db2fc63d1c782d70ba19e
Abhyuday1904 <124274509+Abhyuday1904@users.noreply.github.com>
2024-07-08 15:25:20 +0530

    Time: 4 ms (75.2%), Space: 44.6 MB (85.46%) - LeetHub

---
+++ 1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minOperations(int[] arr) {
+        int ans=0;
+        for(int i=1; i<arr.length; i++){
+            if(arr[i] > arr[i-1]) continue;
+            ans += (arr[i-1]-arr[i]+1);
+            arr[i] = arr[i-1]+1;
+        }
+        return ans;
+    }
+}
