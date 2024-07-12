0e662b8131e6bac053fcb2506e06b4eb6a55cd9b
Abhyuday1904 <124274509+Abhyuday1904@users.noreply.github.com>
2024-07-12 20:31:09 +0530

    Time: 182 ms (51.52%), Space: 48 MB (15.15%) - LeetHub

---
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
@@ -0,0 +1,85 @@
+class Solution {
+    public int maximumGain(String s, int x, int y) {
+        int ans =0;
+        Stack<Character> st = new Stack<>();
+        st.add(s.charAt(0));
+        if(x>=y)
+        {
+            for(int i=1;i<s.length();i++){
+                if(st.size() == 0){
+                    st.add(s.charAt(i));
+                    continue;
+                }
+                if(st.peek() == 'a' && s.charAt(i) == 'b'){
+                    st.pop();
+                    ans += x;
+                } 
+                else st.add(s.charAt(i));
+             //   System.out.println(st);
+            }
+
+            if(st.size()>1){
+                
+            int rem = st.size();
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<rem;i++){
+                sb.append(st.pop());
+                }
+                sb.reverse();
+           // System.out.println(sb);
+            st.add(sb.charAt(0));
+            for(int i=1;i<rem;i++){
+                if(st.size() == 0){
+                    st.add(sb.charAt(i));
+                    continue;
+                }
+                if(st.peek() == 'b' && sb.charAt(i) == 'a'){
+                    st.pop();
+                    ans += y;
+                } 
+                else st.add(sb.charAt(i));
+                //System.out.println(st);
+            }
+            }
+        }
+
+        else {
+            for(int i=1;i<s.length();i++){
+                if(st.size() == 0){
+                    st.add(s.charAt(i));
+                    continue;
+                }
+                if(st.peek() == 'b' && s.charAt(i) == 'a'){
+                    st.pop();
+                    ans += y;
+                } 
+                else st.add(s.charAt(i));
+              //  System.out.println(st);
+            }
+
+            if(st.size()>1){
+                int rem = st.size();
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<rem;i++){
+                sb.append(st.pop());
+                }
+                sb.reverse();
+            //System.out.println(sb);
+            st.add(sb.charAt(0));
+            for(int i=1;i<rem;i++){
+                if(st.size() == 0){
+                    st.add(sb.charAt(i));
+                    continue;
+                }
+                if(st.peek() == 'a' && sb.charAt(i) == 'b'){
+                    st.pop();
+                    ans += x;
+                } 
+                else st.add(sb.charAt(i));
+                //System.out.println(st);
+            }
+            }
+        }
+        return ans;
+    }
+}
