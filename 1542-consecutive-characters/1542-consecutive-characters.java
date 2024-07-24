074212283ed1523ff762cd7962973c5a80646ebc
Abhyuday1904 <124274509+Abhyuday1904@users.noreply.github.com>
2024-07-24 20:32:46 +0530

    Time: 1 ms (100%), Space: 42 MB (31.83%) - LeetHub

--- 1542-consecutive-characters/1542-consecutive-characters.java
+++ 1542-consecutive-characters/1542-consecutive-characters.java
@@ -1,17 +1,16 @@
 class Solution {
     public int maxPower(String s) {
-        int ans =0;
-        
+       int ans =0;
+        int curr =0;
         for(int i=0;i<s.length()-1;i++){
             char ch = s.charAt(i);
-            int curr =0;
-            int j=i+1;
-            while(j<s.length() && s.charAt(j) == ch){
+            if(s.charAt(i+1) == ch){
                 curr++;
-                j++;
+            }
+            else {
+                curr = 0;
             }
             ans = Math.max(ans,curr);
-            // System.out.println(i);
         }
         return ans+1;
     }
