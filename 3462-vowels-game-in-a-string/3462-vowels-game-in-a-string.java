69dc9903d3e0faf577539e73ba34d8aff9db08e5
Abhyuday1904 <124274509+Abhyuday1904@users.noreply.github.com>
2024-07-26 18:45:13 +0530

    Time: 6 ms (84.31%), Space: 45.5 MB (69.39%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        int n = s.length(); int count=0;
+        for(char ch : s.toCharArray()){
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') count++;
+        }
+        if(count==0) return false;
+        return true;
+    }
+}
