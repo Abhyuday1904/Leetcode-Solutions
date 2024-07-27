4081c39e7859a110c3179e41a6afd3c83d451628
Abhyuday1904 <124274509+Abhyuday1904@users.noreply.github.com>
2024-07-27 20:52:24 +0530

    Time: 6 ms (56.74%), Space: 45.5 MB (39.36%) - LeetHub

--- 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.java
+++ 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.java
@@ -1,33 +1,29 @@
 class Solution {
-    public int[] findIntersectionValues(int[] arr1, int[] arr2) {
-        int ans[] =  new int[2];
-        HashMap<Integer,Integer> map = new HashMap<>();
-        HashMap<Integer,Integer> nap = new HashMap<>();
-        for(int i=0;i<arr1.length;i++){
-            if(!map.containsKey(arr1[i])) map.put(arr1[i],1);
-            else map.put(arr1[i],map.get(arr1[i])+1);
+    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+         
+        HashSet<Integer> hs1 = new HashSet<>();
+        HashSet<Integer> hs2 = new HashSet<>();
+        
+        for(int i=0;i<nums1.length;i++) hs1.add(nums1[i]);
+        for(int i=0;i<nums2.length;i++) hs2.add(nums2[i]);
+        
+        int ans[]= new int[2];
+       
+        int count=0;
+        for(int i=0;i<nums1.length;i++)
+        {
+           if(hs2.contains(nums1[i])) count++;  
         }
-        for(int i=0;i<arr2.length;i++){
-            if(!nap.containsKey(arr2[i])) nap.put(arr2[i],1);
-            else nap.put(arr2[i],nap.get(arr2[i])+1);
+        ans[0]=count;
+        count=0;
+        
+        for(int i=0;i<nums2.length;i++)
+        {
+           if(hs1.contains(nums2[i])) count++;  
         }
-
-        int first = 0;
-        int second  = 0;
-        for(int a : map.keySet()){
-            // System.out.println("a ->" +a);
-            if(nap.containsKey(a)){
-                first += map.get(a);
-            }
-            //System.out.println("first ->"+first);
-        }
-        for(int a : nap.keySet()){
-            if(map.containsKey(a)){
-                second += nap.get(a);
-            }
-        }
-        ans[0]  = first;
-        ans[1] = second;
+        ans[1]=count;
         return ans;
+        
+        
     }
 }
