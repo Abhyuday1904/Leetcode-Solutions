23dd4b1d061725f12509a38f242cf71cf4007c85
Abhyuday1904 <124274509+Abhyuday1904@users.noreply.github.com>
2024-08-14 21:57:03 +0530

    Time: 7 ms (67.5%), Space: 47.8 MB (52.5%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,41 +1,19 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> arr = new ArrayList<>();
-        HashSet<String> set = new HashSet<>();
-       HashMap<String,Integer > napp = new HashMap<>();
-       HashMap<String,String> map = new HashMap<>();
-        // String st[] = strs;
-        int len = strs.length;
+        Map<String, List<String>> map = new HashMap<>();
         
-        for (int i = 0; i < len; i++) {
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
             
-            char[] charArray = strs[i].toCharArray();
-            Arrays.sort(charArray);
-
-            
-           String st = new String(charArray);
-           set.add(st);
-           map.put(strs[i], st);
-           if(!napp.containsKey(strs[i])) napp.put(strs[i],1);
-           else napp.put(strs[i],napp.get(strs[i])+1);
-        }
-
-        for(String st : set){
-            ArrayList<String> list = new ArrayList<>();
-            for(String s : map.keySet()){
-                
-                if(map.get(s).equals(st)) 
-                {
-                    int k = napp.get(s);
-                    while(k>0){
-                        list.add(s);
-                        k--;
-                    }
-                    
-                }
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
             }
-            arr.add(list);
+            
+            map.get(sortedWord).add(word);
         }
-        return arr;
+        
+        return new ArrayList<>(map.values());
     }
 }
