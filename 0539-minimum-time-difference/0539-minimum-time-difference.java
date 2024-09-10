73c04790272adf4b218e5409470e0742f45e3948
Abhyuday1904 <124274509+Abhyuday1904@users.noreply.github.com>
2024-09-10 17:22:13 +0530

    Time: 6 ms (54.61%), Space: 44.6 MB (97.94%) - LeetHub

---
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int findMinDifference(List<String> t) {
+        int a[] = new int[t.size()];
+        for(int i=0;i<a.length;i++){
+            a[i] = Integer.parseInt(t.get(i).substring(0,2)) * 60 + Integer.parseInt(t.get(i).substring(3,5));
+        
+        }
+        Arrays.sort(a);
+        int min = Integer.MAX_VALUE;
+     //   System.out.println(Arrays.toString(a));
+        for(int i=0;i<a.length-1;i++){
+            if(a[i+1] - a[i] >= 0){
+                min = Math.min(a[i+1] - a[i],min);
+            }
+            
+        }
+        for(int i=0;i<a.length-1;i++){
+            if(a[i] <= 720){
+                a[i] += 1440;
+            }
+        }
+         Arrays.sort(a);
+
+       //   System.out.println(Arrays.toString(a));
+         for(int i=0;i<a.length-1;i++){
+            if(a[i+1] - a[i] >= 0){
+                min = Math.min(a[i+1] - a[i],min);
+            }
+            
+        }
+       // System.out.println(Arrays.toString(a));
+        return min;
+    }
+}
