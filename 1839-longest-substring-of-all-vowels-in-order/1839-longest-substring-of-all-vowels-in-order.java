97ed8362778821a74b7a78eae5e894355da2598d
Abhyuday1904 <124274509+Abhyuday1904@users.noreply.github.com>
2024-10-01 23:52:23 +0530

    Time: 13 ms (90.07%), Space: 47.9 MB (81.99%) - LeetHub

---
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int longestBeautifulSubstring(String s) {
+      int maxi = 0;
+      int startIdx = 0;
+      for(int i = 0 ; i < s.length() - 1 ; i++) {
+        boolean aflg = false;
+        boolean eflg = false;
+        boolean iflg = false;
+        boolean oflg = false;
+        boolean uflg = false;
+        if(s.charAt(i) == 'a') {
+          startIdx = i;
+          aflg = true;
+          while(i + 1 < s.length() && s.charAt(i+1) == 'a') {
+            i++;
+          }
+          if(i + 1 < s.length() && s.charAt(i+1) != 'e') continue;
+          while(i + 1 < s.length() && s.charAt(i+1) == 'e') {
+            eflg = true;
+            i++;
+          }
+          if(i + 1 < s.length() && s.charAt(i+1) != 'i') continue;
+          while(i + 1 < s.length() && s.charAt(i+1) == 'i') {
+            iflg = true;
+            i++;
+          }
+          if(i + 1 < s.length() && s.charAt(i+1) != 'o') continue;
+          while(i + 1 < s.length() && s.charAt(i+1) == 'o') {
+            oflg = true;
+            i++;
+          }
+          if(i + 1 < s.length() && s.charAt(i+1) != 'u') continue;
+          while(i + 1 < s.length() && s.charAt(i+1) == 'u') {
+            uflg = true;
+            i++;
+          }
+          if(aflg && eflg && iflg && oflg && uflg) {
+            maxi = Math.max(maxi, i - startIdx + 1);
+          }
+        }
+      }
+      return maxi;
+    }
+}
