e7c9770066288b8e136a2c76e2b5352c99deea1e
Abhyuday1904 <124274509+Abhyuday1904@users.noreply.github.com>
2024-10-02 12:14:54 +0530

    Time: 96 ms (5.21%), Space: 59.2 MB (85.45%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        TreeMap<Integer , Integer> map = new TreeMap<>();
+        for(int a : arr){
+            map.put(a, map.getOrDefault(a , 0) + 1);
+        }
+        int idx = 1;
+        for(int num : map.keySet()){
+            map.put(num , idx++);
+        }
+        int ans[]  = new int[arr.length];
+        for(int i=0;i<arr.length ;i++){
+            ans[i] = map.get(arr[i]);
+        }
+        return ans;
+    }
+}
