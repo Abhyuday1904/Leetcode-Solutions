131efbd93846d9e76a2c8aa1ced80bc7455b1f50
Abhyuday1904 <124274509+Abhyuday1904@users.noreply.github.com>
2025-01-19 10:46:54 +0530

    Time: 19 ms (88.09%), Space: 51.3 MB (20.5%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,20 +1,26 @@
 class Solution {
     public int longestCommonSubsequence(String s, String t) {
-        int dp[][] = new int[s.length()][t.length()];
+        int dp[][] = new int[s.length()+1][t.length()+1];
         for(int i =0 ;i<dp.length;i++){
             for(int j = 0;j<dp[0].length;j++){
                 dp[i][j] = -1;
             }
         }
-        return solve(s, t , s.length()-1 , t.length() - 1 , dp);
-    }
-    public static int solve(String s , String t , int i , int j , int dp[][]){
-        if(i<0 || j<0 ) return 0;
-        if(dp[i][j]!= -1) return dp[i][j];
-        if(s.charAt(i) == t.charAt(j)) 
-        return dp[i][j] =  1 + solve(s , t , i-1 , j-1 , dp);
-        else{
-        return dp[i][j]  = Math.max(solve(s, t,i-1 , j ,dp) , solve(s,t, i , j-1 ,dp));
+         for(int i =0 ;i<dp.length;i++ ) dp[i][0] = 0;
+       for(int j =0 ;j<dp[0].length;j++) dp[0][j] = 0;
+
+       for(int i = 1;i<dp.length;i++){
+        for(int j = 1;j<dp[0].length;j++){
+
+            if(s.charAt(i-1) == t.charAt(j-1)){
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+            else{
+               dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+            }
         }
+       }
+       return dp[s.length()][t.length()];
     }
+    
 }
