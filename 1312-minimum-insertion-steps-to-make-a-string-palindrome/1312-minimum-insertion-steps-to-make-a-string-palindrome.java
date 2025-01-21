cc5845382e306e2df8b4e22aeead2a3435582176
Abhyuday1904 <124274509+Abhyuday1904@users.noreply.github.com>
2025-01-21 11:29:37 +0530

    Time: 27 ms (44.03%), Space: 45.7 MB (37.11%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minInsertions(String s) {
+        if(s.length() == 1 )return 0;
+         if(s.length() == 1 ) return 1;
+        StringBuilder ss = new StringBuilder(s);
+        return s.length() - longestCommonSubsequence(s , ss.reverse().toString());
+    }
+     public int longestCommonSubsequence(String s, String t) {
+        int dp[][] = new int[s.length()+1][t.length()+1];
+        for(int i =0 ;i<dp.length;i++){
+            for(int j = 0;j<dp[0].length;j++){
+                dp[i][j] = -1;
+            }
+        }
+         for(int i =0 ;i<dp.length;i++ ) dp[i][0] = 0;
+       for(int j =0 ;j<dp[0].length;j++) dp[0][j] = 0;
+
+       for(int i = 1;i<dp.length;i++){
+        for(int j = 1;j<dp[0].length;j++){
+
+            if(s.charAt(i-1) == t.charAt(j-1)){
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+            else{
+               dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+            }
+        }
+       }
+       return dp[s.length()][t.length()];
+    }
+}
